e1bb4c51ce38ac765b25ba186b3f4679e747f94e
Yash-K-B <yashwantkumar027@gmail.com>
2020-12-07 09:31:25 +0530

    fix

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,13 +8,13 @@ from allauth import app_settings
 
 
 urlpatterns = [
-    path('login',views.user_login,name="login"),
-    path('logout',views.user_logout,name="logout"),
-    path('signup',views.signup,name="signup"),
-    path('password_reset',auth_views.PasswordResetView.as_view(html_email_template_name='registration/password_reset_email.html'),name="password_reset"),
-    path('password_reset/done',auth_views.PasswordResetDoneView.as_view(),name="password_reset_done"),
-    path('reset/<uidb64>/<token>',auth_views.PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
-    path('reset/done',auth_views.PasswordResetCompleteView.as_view(),name="password_reset_complete"),
+    path('login/',views.user_login,name="login"),
+    path('logout/',views.user_logout,name="logout"),
+    path('signup/',views.signup,name="signup"),
+    path('password_reset/',auth_views.PasswordResetView.as_view(html_email_template_name='registration/password_reset_email.html'),name="password_reset"),
+    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(),name="password_reset_done"),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
+    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name="password_reset_complete"),
 ]
 
 
