2f5e1f8d78a13e0831e156a92f7de4a755da0b46
Nachiketa Dhal <bnachiketa26@gmail.com>
2021-05-09 13:44:51 +0530

    Fix minor bugs

--- static/js/subject.js
+++ static/js/subject.js
@@ -1,13 +1,13 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "100%";
-    document.getElementById("main").style.marginLeft = "100%";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  document.getElementById("mySidenav").style.width = "100%";
+  document.getElementById("main").style.marginLeft = "100%";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = "white";
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.body.style.backgroundColor = "white";
 }
 
 /////////////////////////////// Filter Subjects ////////////////////////////
@@ -15,39 +15,39 @@ function closeNav() {
 filterSelection("all");
 
 function filterSelection(c) {
-    var x, i;
-    x = document.getElementsByClassName("filterDiv");
-    if (c == "all") c = "";
-    // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-    for (i = 0; i < x.length; i++) {
-        removeClass(x[i], "show");
-        if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
-    }
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    removeClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+  }
 }
 
 // Show filtered elements
 function addClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-        if (arr1.indexOf(arr2[i]) == -1) {
-            element.className += " " + arr2[i];
-        }
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
     }
+  }
 }
 
 // Hide elements that are not selected
 function removeClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-        while (arr1.indexOf(arr2[i]) > -1) {
-            arr1.splice(arr1.indexOf(arr2[i]), 1);
-        }
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
     }
-    element.className = arr1.join(" ");
+  }
+  element.className = arr1.join(" ");
 }
 
 // Customize the dropdown
@@ -55,51 +55,56 @@ const dropdownToggle = document.querySelector(".dropdown-toggle");
 const dropdownItem = document.getElementsByClassName("dropdown-item");
 
 for (let i = 0; i < dropdownItem.length; i++) {
-    dropdownItem[i].addEventListener("click", () => {
-        dropdownToggle.innerHTML = dropdownItem[i].innerHTML;
-    });
+  dropdownItem[i].addEventListener("click", () => {
+    dropdownToggle.innerHTML = dropdownItem[i].innerHTML;
+  });
 }
 
-
-$(".search-form").on("submit", function(e) { //prevent from enter key hit
-    e.preventDefault();
-
+$(".search-form").on("submit", function (e) {
+  //prevent from enter key hit
+  e.preventDefault();
 });
 
 // Search implementation
 const searchSubjects = () => {
+  const input = document.querySelector(".form-control.search");
+  const inputValue = input.value.toLowerCase();
+  // console.log(inputValue.trim());
+  const subject = document.querySelectorAll(".bg-clip span"); // Array
+  const subjectDiv = document.querySelectorAll(".bg-clip"); // Array
+  const subjectShortcut = document.querySelectorAll("div.bg-clip"); // Array
+  // console.log(subject[0]);
+  // console.log(subjectDiv[0]);
+  let subjectValue;
+  let subjectShortcutValue;
 
-    const input = document.querySelector(".form-control.search");
-    const inputValue = input.value.toLowerCase();
-    // console.log(inputValue.trim());
-    const subject = document.querySelectorAll(".bg-clip span"); // Array
-    const subjectDiv = document.querySelectorAll(".bg-clip"); // Array
-    const subjectShortcut = document.querySelectorAll("div.bg-clip"); // Array
-    // console.log(subject[0]);
-    // console.log(subjectDiv[0]);
-    let subjectValue;
-    let subjectShortcutValue;
+  for (let i = 0; i < subject.length; i++) {
+    subjectValue = subject[i].innerText.toLowerCase();
+    if (subjectShortcut[i] == null) continue;
+    subjectShortcutValue = subjectShortcut[i].innerText
+      .split("\n")[0]
+      .toLowerCase();
+    // console.log(subjectShortcutValue); // [m-i, phy, be, ...]
 
-    for (let i = 0; i < subject.length; i++) {
-        subjectValue = subject[i].innerText.toLowerCase();
-        if (subjectShortcut[i] == null) continue;
-        subjectShortcutValue = subjectShortcut[i].innerText
-            .split("\n")[0]
-            .toLowerCase();
-        // console.log(subjectShortcutValue); // [m-i, phy, be, ...]
-
-
-        if (
-            subjectValue.indexOf(inputValue.trim()) > -1 ||
-            subjectShortcutValue.indexOf(inputValue.trim()) > -1
-        ) {
-            subjectDiv[i].style.display = "";
-            // console.log(subjectValue + " " + subjectShortcutValue);
-            //console.log();
-        } else {
-            subjectDiv[i].style.setProperty("display", "none", "important");
-        }
+    if (
+      subjectValue.indexOf(inputValue.trim()) > -1 ||
+      subjectShortcutValue.indexOf(inputValue.trim()) > -1
+    ) {
+      subjectDiv[i].style.display = "";
+      // console.log(subjectValue + " " + subjectShortcutValue);
+      //console.log();
+    } else {
+      subjectDiv[i].style.setProperty("display", "none", "important");
     }
+  }
 };
 
 // console.log(subjectShortcut[0].innerText.split("\n")[0]);
+
+// Prevents page redirecting upon selecting
+const dropDown = document.querySelectorAll(".dropdown-item");
+dropDown.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    e.preventDefault();
+  });
+});
